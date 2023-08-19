001b78ef53935bb7fc28e988fa4db08e4fe6ac6e
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-19 22:03:32 +0300

    Update current.js

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -2,28 +2,26 @@
 const { Pet } = require("../../models/pet");
 
 const current = async (req, res) => {
-    const { email, name, phone, birthday, city, avatarURL } = req.user;
-    const {_id: owner} = req.user;
-    const ownerID = {owner};
-    const result =  await Pet.find(ownerID, "-createdAt -updatedAt").populate(
-      "owner",
-      "name"
-    );
-  
+    const { email, name, phone, city, birthday, avatarURL } = req.user;
+    const { _id: owner } = req.user;
+    const ownerID = { owner };
+    const result = await Pet.find(ownerID, "-createdAt -updatedAt").populate(
+          "owner",
+          "name"
+       );
     res.status(200).json({
-      code:200,
-      status: "success",
-      user: {
-      name,
-      email,
-      phone,
-      city,
-      birthday,
-      avatarURL
+      code: 200,
+      status:"success",
+      user:{
+        name,
+        email,
+        phone,
+        city,
+        birthday,
+        avatarURL
       },
       pets: [...result],  
       
     });
   };
-
 module.exports = current;
