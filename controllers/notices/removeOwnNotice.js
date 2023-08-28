942883cca665c97435d0c0ec1d9e45777a25e868
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-08-28 12:38:15 +0300

    Add fixes

--- controllers/notices/removeOwnNotice.js
+++ controllers/notices/removeOwnNotice.js
@@ -1,18 +1,25 @@
 const { Notice } = require("../../models/notice");
-const {HttpError} = require("../../helpers");
+const { HttpError } = require("../../helpers");
 
-const removeOwnNotice = async (req, res) => { 
+const removeOwnNotice = async (req, res) => {
   const { noticeId } = req.params;
-  
-  const result = await Notice.findByIdAndRemove(noticeId);
-  
-  if (!result){
+  const { _id: owner } = req.user;
+
+  const result = await Notice.findById(noticeId);
+
+  if (!result) {
     throw HttpError(404, "Not found");
   }
-  
-  res.status(204).json({
-    message: "Notice deleted"
-  })
+
+  if (result.owner !== owner) {
+    throw HttpError(403, "You can't delete other users notice");
+  }
+
+  await result.remove();
+
+  res.json({
+    message: "Notice deleted",
+  });
 };
 
 module.exports = removeOwnNotice;
--- controllers/pets/pets.js
+++ controllers/pets/pets.js
@@ -1,35 +1,39 @@
-
 const { HttpError, ctrlWrapper } = require("../../helpers");
 const { Pet } = require("../../models/pet");
 
 // add pet
 const addPet = async (req, res) => {
-const { _id: owner } = req.user;
- const imageURL = req.file.path;
+  const { _id: owner } = req.user;
+  const imageURL = req.file.path;
 
   const result = await Pet.create({ ...req.body, imageURL, owner });
 
   res.status(201).json({
-      code: 201,
-      status: "success",
-      pet: result,
-      }
-    );
+    code: 201,
+    status: "success",
+    pet: result,
+  });
 };
 
 // delete pet
 const removePet = async (req, res) => {
   const { petId } = req.params;
+  const { _id: owner } = req.user;
 
-  const result = await Pet.findByIdAndRemove(petId);
+  const result = await Pet.findById(petId);
 
   if (!result) {
     throw HttpError(404, "Pet with this id not found");
-  };
+  }
+
+  if (result.owner !== owner) {
+    throw HttpError(403, "You can't delete other users pet's card");
+  }
+
+  await result.remove();
 
   res.json({
     message: "Delete success",
-    id: petId,
   });
 };
 
