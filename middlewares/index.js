176325008d2f617e258f64f93ebb1d61911f371b
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-28 19:45:03 +0300

    fix bugs

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,12 +1,10 @@
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
 const validateBody = require("./validateBody");
-const isValidIdPet = require("./isValidIdPet");
 const upload = require("./upload");
 
 module.exports = {
   isValidId,
-  isValidIdPet,
   authenticate,
   validateBody,
   upload,
