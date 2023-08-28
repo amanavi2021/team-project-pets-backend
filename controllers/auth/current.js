8300cd54eec14c7b377e8b9070d45410094ad9d1
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-28 18:37:28 +0300

    adds favorites in login

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,32 +1,31 @@
-// get current user date 
+// get current user date
 const { Pet } = require("../../models/pet");
 
 const current = async (req, res) => {
-    const { email, name, phone, city, birthday, avatarURL, favorite } = req.user;
-    const { _id: owner } = req.user;
-  
-    const ownerID = { owner };
-  
-    const result = await Pet.find(ownerID, "-createdAt -updatedAt").populate(
-          "owner",
-          "name"
-    );
-  
-    res.status(200).json({
-      code: 200,
-      status:"success",
-      user:{
-        name,
-        email,
-        phone,
-        city,
-        birthday,
-        avatarURL,
-        favorite
-      },
-      pets: [...result],  
-      
-    });
+  const { email, name, phone, city, birthday, avatarURL, favorite } = req.user;
+  const { _id: owner } = req.user;
+
+  const ownerID = { owner };
+
+  const result = await Pet.find(ownerID, "-createdAt -updatedAt").populate(
+    "owner",
+    "name"
+  );
+
+  res.status(200).json({
+    code: 200,
+    status: "success",
+    user: {
+      name,
+      email,
+      phone,
+      city,
+      birthday,
+      avatarURL,
+      favorite,
+    },
+    pets: [...result],
+  });
 };
-  
+
 module.exports = current;
--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,46 +1,46 @@
 const bcrypt = require("bcrypt");
 
-const { User } = require('../../models/user');
-const { tokens } = require('../../helpers/tokens');
-const { HttpError} = require('../../helpers');
-
+const { User } = require("../../models/user");
+const { tokens } = require("../../helpers/tokens");
+const { HttpError } = require("../../helpers");
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    
-    if (!user) {
-        throw HttpError(409, "User with this email not found");
-    };
-    
-    const passwordCompare = bcrypt.compare(password, user.password);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (!passwordCompare) {
-        throw HttpError(401, "Email or password is wrong");
-    };
+  if (!user) {
+    throw HttpError(409, "User with this email not found");
+  }
 
-    const { token, refreshToken } = await tokens(user._id);
+  const passwordCompare = bcrypt.compare(password, user.password);
 
-    await User.findByIdAndUpdate(user._id, { token, refreshToken });
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
 
-    res.cookie('refreshToken', refreshToken, {
-        httpOnly: true,
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+  const { token, refreshToken } = await tokens(user._id);
 
-    res.status(200).json({
-        code: 200,
-        status: "success",
-        token,
-        user: {
-            email: user.email,
-            name: user.name,
-            phone: user.phone,
-            city: user.city,
-            birthday: user.birthday,
-            avatarURL: user.avatarURL,
-        },
-    });
+  await User.findByIdAndUpdate(user._id, { token, refreshToken });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    code: 200,
+    status: "success",
+    token,
+    user: {
+      email: user.email,
+      name: user.name,
+      phone: user.phone,
+      city: user.city,
+      birthday: user.birthday,
+      avatarURL: user.avatarURL,
+      favorite: user.favorite,
+    },
+  });
 };
 
 module.exports = login;
