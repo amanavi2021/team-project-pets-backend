a2931a97ecde11b432134712c8905fa4fe9ef934
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-28 19:40:49 +0300

    Update update.js

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -1,12 +1,12 @@
-const { User } = require('../../models/user');
+const { User } = require("../../models/user");
 
+const update = async (req, res) => {
+  const { _id } = req.user;
+  const { name, email, birthday, phone, city } = req.body;
+  const avatarURL = req.file ? req.file.path : req.user.avatarURL;
 
-const update = async(req, res) => {
-    const { _id } = req.user;
-    const { name, email, birthday, phone, city } = req.body;
-    const avatarURL = req.file ? req.file.path : req.user.avatarURL;
-  
-  const updateData = await User.findOneAndUpdate(_id,
+  const updateData = await User.findOneAndUpdate(
+    _id,
     {
       name,
       email,
@@ -19,21 +19,19 @@ const update = async(req, res) => {
       new: true,
     }
   );
-    
-    res.json({
-        code: 200,
-        status: 'success',
-        user: {
-            name: updateData.name,
-            email: updateData.email,
-            phone: updateData.phone,
-            city: updateData.city,
-            birthday: updateData.birthday,
-            avatarURL: updateData.avatarURL,
-        },
-    });
+
+  res.json({
+    code: 200,
+    status: "success",
+    user: {
+      name: updateData.name,
+      email: updateData.email,
+      phone: updateData.phone,
+      city: updateData.city,
+      birthday: updateData.birthday,
+      avatarURL: updateData.avatarURL,
+    },
+  });
 };
-
-
 
 module.exports = update;
